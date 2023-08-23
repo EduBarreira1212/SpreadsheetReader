27cb0834da16e6c97d56f9414653754886814306
Eduardo Barreira <edubarreira1212@gmail.com>
2023-08-23 01:58:17 -0300

    Function to get the rows of spreadsheet and turn into Object

--- index.js
+++ index.js
@@ -19,3 +19,11 @@ async function GetDoc() {
     return doc;
 }
 
+async function ReadWorkSheet() {
+    let sheet = (await GetDoc()).sheetsByIndex[0];
+    let rows = await sheet.getRows();
+    let users = rows.map(row => {
+        return row.toObject();
+    })
+    return users;
+}
