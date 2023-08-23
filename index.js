a7562e6a360aefa9982c92a9186a15ea2bd340ff
Eduardo Barreira <edubarreira1212@gmail.com>
2023-08-23 01:39:42 -0300

    Inisial files and exports/imports created

---
+++ index.js
@@ -0,0 +1,6 @@
+const credential = require("./credentials.json");
+const arquivo = require("./arquivo.json");
+const { JWT } = require("google-auth-library");
+const { GoogleSpreadsheet } = require("google-spreadsheet");
+
+
